cbdfb9b9d512586600cd55903411ca50f2b64a3d
Vivek Jayachandran <vivek4integration@gmail.com>
2023-07-18 19:29:45 +1200

    Adding comments to code.

--- Lesson 2/Spiral.py
+++ Lesson 2/Spiral.py
@@ -2,9 +2,11 @@ import turtle
 loadWindow = turtle.Screen()
 turtle.speed(0)
  
+# loop and create circle
 for i in range(100):
     turtle.circle(5*i)
     turtle.circle(-5*i)
     turtle.left(i)
- 
+
+#  make the window exit when clicked
 turtle.exitonclick()
--- Lesson 2/Spiral2.py
+++ Lesson 2/Spiral2.py
@@ -1,8 +1,11 @@
 import turtle
 colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
 t = turtle.Pen()
+# Set background color
 turtle.bgcolor('black')
 turtle.speed(0)
+
+# Loop and create line of different color
 for x in range(360):
     t.pencolor(colors[x%6])
     t.width(x//100 + 1)
